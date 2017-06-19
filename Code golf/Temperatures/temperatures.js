80cd34a687aa356ec2981df3a9db0892fe2ace58
Johan Brunet <brunet.johan.23@gmail.com>
2017-06-19 16:26:32 +0200

    New best for "Temperatures" code golf
    Reached 128 chars

--- Code golf/Temperatures/temperatures.js
+++ Code golf/Temperatures/temperatures.js
@@ -1,2 +1,2 @@
 m=Math.abs;n=readline();(t=readline())!=0?(t=t.split(" "),r=t[0]):r=0
-for(j=0;j<n;j++){k=t[j];l=m(r);z=m(k);if(l>z||(l==z&&r<k))r=k}print(r)
+for(j of t){l=m(r);z=m(j);if(l>z||(l==z&&r<j))r=j}print(r)
