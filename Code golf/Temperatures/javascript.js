b1631cdcdde9cf584ffe82835c7f229f621dace0
Johan Brunet <brunet.johan.23@gmail.com>
2017-06-19 16:49:10 +0200

    Rename all files after language name

--- Code golf/Temperatures/javascript.js
+++ Code golf/Temperatures/javascript.js
@@ -1,2 +1,2 @@
 m=Math.abs;n=readline();(t=readline())!=0?(t=t.split(" "),r=t[0]):r=0
-for(j of t){l=m(r);z=m(j);if(l>z||(l==z&&r<j))r=j}print(r)
+for(j of t){l=m(r);z=m(j);if(l>z||l==z&&r<j)r=j}print(r)
