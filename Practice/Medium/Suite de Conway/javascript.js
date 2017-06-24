a1509b70c22073e777ff93d9fd9cb6be81619552
Johan Brunet <brunet.johan.23@gmail.com>
2017-06-24 13:26:57 +0200

    [add] Practice medium
    Only 50%

---
+++ Practice/Medium/Suite de Conway/javascript.js
@@ -0,0 +1,33 @@
+var R = parseInt(readline());
+var L = parseInt(readline());
+
+ligneSuiv = (ligne) => {
+    let suiv = []
+    let lastC = ligne[0]
+    let cpt = 0
+    for (let c of ligne) {
+        if (c == lastC) {
+            cpt++
+        }
+        else {
+            suiv.push(cpt.toString())
+            suiv.push(lastC)
+            lastC = c
+            cpt = 1
+        }
+    }
+    suiv.push(cpt.toString())
+    suiv.push(lastC)
+    return suiv
+}
+
+conway = (n) => {
+    let ligne = R.toString();
+    for (let i = 0; i < n; i++) {
+        ligne = ligneSuiv(ligne)
+        // print(ligne.join(" "))
+    }
+    print(ligne.join(" "))
+}
+
+conway(L-1);
